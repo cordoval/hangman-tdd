2aaf7838df34a11501a461818649d680e494a4f8
Luis Cordova <cordoval@gmail.com>
2015-02-02 15:52:48 -0500

    failing: add test for when we have one to go and we miss the guess

--- tests/EndTest.php
+++ tests/EndTest.php
@@ -21,4 +21,19 @@ class EndTest extends \PHPUnit_Framework_TestCase
         $game->guessCharacter('d');
         $this->assertEquals(Api::GAME_SUCCESS, $game->getStatus());
     }
+
+    /** @test */
+    public function it_ends_the_game_failing_when_one_letter_is_missing_and_letter_guessed_is_not_correct()
+    {
+        $game = Api::bootGame();
+        $this->assertEquals('someword', $game->getWord());
+        foreach (str_split('abcfghijkl') as $character) {
+            $game->guessCharacter($character);
+        }
+        $this->assertEquals(1, $game->getTriesLeft());
+        $this->assertEquals(Api::GAME_BUSY, $game->getStatus());
+
+        $game->guessCharacter('n');
+        $this->assertEquals(Api::GAME_FAIL, $game->getStatus());
+    }
 }
