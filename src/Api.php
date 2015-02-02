8150636e017fffe83f8fb9fea593102a4e6a8b7b
Luis Cordova <cordoval@gmail.com>
2015-02-02 09:42:11 -0500

    fail bootstrapping a game

---
+++ src/Api.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Qandidate;
+
+class Api
+{
+    public static function bootGame()
+    {
+        return new self();
+    }
+}
---
+++ tests/StartTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Qandidate\Tests;
+
+use Qandidate\Api;
+
+class StartTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_starts_a_game()
+    {
+        $game = Api::bootGame();
+        $this->assertTrue(is_int($game->getWord()));
+    }
+}
