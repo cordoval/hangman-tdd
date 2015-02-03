0d718c29b9b0e0267d6f07ba10261ccdb8d65f2d
Luis Cordova <cordoval@gmail.com>
2015-02-02 21:00:02 -0500

    failing: object unable to fetch first word from huge word list file

--- src/WordList.php
+++ src/WordList.php
@@ -4,5 +4,20 @@ namespace Qandidate;
 
 class WordList
 {
+    const DATA_PATH = __DIR__.'/../data/words.english';
 
+    public static function boot($dataPath = self::DATA_PATH)
+    {
+        return new self($dataPath);
+    }
+
+    public function getWordAt()
+    {
+
+    }
+
+    private function __construct($dataPath)
+    {
+
+    }
 }
--- tests/WordList/AccessTest.php
+++ tests/WordList/AccessTest.php
@@ -4,5 +4,10 @@ namespace Qandidate;
 
 class AccessTest extends \PHPUnit_Framework_TestCase
 {
-
+    /** @test */
+    public function it_fetches_first_word_from_large_file()
+    {
+        $wordList = WordList::boot();
+        $this->assertEquals('aa', $wordList->getWordAt(0));
+    }
 }
