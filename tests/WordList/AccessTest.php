1f63350f9e8cbe89611bb023ec6bcc9e9528addd
Luis Cordova <cordoval@gmail.com>
2015-02-02 21:05:04 -0500

    failing: cannot fetch third word of the huge word list

--- tests/WordList/AccessTest.php
+++ tests/WordList/AccessTest.php
@@ -10,4 +10,11 @@ class AccessTest extends \PHPUnit_Framework_TestCase
         $wordList = WordList::boot();
         $this->assertEquals('aa', $wordList->getWordAt(0));
     }
+
+    /** @test */
+    public function it_fetches_third_word_from_large_file()
+    {
+        $wordList = WordList::boot();
+        $this->assertEquals('aahing', $wordList->getWordAt(2));
+    }
 }
