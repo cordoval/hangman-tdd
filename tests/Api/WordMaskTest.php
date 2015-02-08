40415f82561fb507d8f834b6f84bf1b8e5c3fbda
Luis Cordova <cordoval@gmail.com>
2015-02-08 18:56:18 -0500

    failing: mask is not yet implemented

--- tests/Api/WordMaskTest.php
+++ tests/Api/WordMaskTest.php
@@ -2,6 +2,7 @@
 
 namespace Qandidate\Tests\Api;
 
+use Qandidate\Mask;
 use Qandidate\Word;
 
 class WordMaskTest extends \PHPUnit_Framework_TestCase
@@ -14,5 +15,35 @@ class WordMaskTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(8, $word->getLength());
         $this->assertTrue($word->hasCharacter('e'));
         $this->assertFalse($word->hasCharacter('f'));
+    }
+
+    /** @test */
+    public function it_represents_a_mask()
+    {
+        $word = new Word('someword');
+        $mask = new Mask($word);
+        $this->assertEquals('someword', (string) $mask->getWord());
+        $this->assertEquals($word->getLength(), $mask->getLength());
+        $this->assertEquals('........', (string) $mask);
+        $this->assertTrue($mask->hasUnknownCharacters());
+
+        $secondMask = $mask->unveilCharacter('o');
+        $this->assertEquals('.o...o..', (string) $secondMask);
+        $this->assertEquals($mask->getword(), $secondMask->getWord());
+        $this->assertEquals($mask->getLength(), $secondMask->getLength());
+
+        $thirdMask = $secondMask->unveilCharacter('o');
+        $this->assertEquals('.o...o..', (string) $thirdMask);
+        $this->assertEquals($thirdMask->getword(), $secondMask->getWord());
+        $this->assertEquals($thirdMask->getLength(), $secondMask->getLength());
+
+        $fourthMask = $thirdMask->unveilCharacter('s');
+        $fifthMask = $fourthMask->unveilCharacter('m');
+        $sixthMask = $fifthMask->unveilCharacter('e');
+        $seventhMask = $sixthMask->unveilCharacter('w');
+        $eightMask = $seventhMask->unveilCharacter('r');
+        $nineMask = $eightMask->unveilCharacter('d');
+
+        $this->assertFalse($mask->hasUnknownCharacters());
     }
 }
