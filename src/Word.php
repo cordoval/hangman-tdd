b68f74cc5b680aa1c1c65259d0e5c1e546778a81
Luis Cordova <cordoval@gmail.com>
2015-02-08 18:38:04 -0500

    passing: word is now a representation that has util methods

--- src/Word.php
+++ src/Word.php
@@ -4,5 +4,25 @@ namespace Qandidate;
 
 class Word
 {
+    private $value;
 
+    public function __construct($nativeRepresentation)
+    {
+        $this->value = str_split($nativeRepresentation);
+    }
+
+    public function __toString()
+    {
+        return implode('', $this->value);
+    }
+
+    public function getLength()
+    {
+        return sizeof($this->value);
+    }
+
+    public function hasCharacter($character)
+    {
+        return in_array($character, $this->value);
+    }
 }
