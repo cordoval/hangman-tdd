c83642c95cf12ee2089c2630ff6c222e783c203b
Luis Cordova <cordoval@gmail.com>
2015-02-08 22:25:59 -0500

    plug bootstrap cache and update some old namespaces

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,7 +11,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-$loader = require_once __DIR__.'/../vendor/autoload.php';
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 Debug::enable();
 $kernel = new AppKernel('dev', true);
